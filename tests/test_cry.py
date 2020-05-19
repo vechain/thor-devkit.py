ecd210b200525dac86b1abb87c1ecd346ff925a7
laalaguer <laalaguer@gmail.com>
2020-05-19 17:23:03 +0800

    restructure project

--- tests/test_cry.py
+++ tests/test_cry.py
@@ -1,27 +1,22 @@
-from cry import blake2b256
-from cry import keccak256
-from cry import is_address
-from cry import to_checksum_address
-from cry import public_key_to_address
-from cry import secp256k1
-from cry import mnemonic
-from cry import keystore
-from cry import HDNode
+from thor_devkit import cry
+from thor_devkit.cry import secp256k1
+from thor_devkit.cry import mnemonic
+from thor_devkit.cry import keystore
 
 
 def test_blake2b():
-    h, _ = blake2b256([b'hello world'])
+    h, _ = cry.blake2b256([b'hello world'])
     assert h.hex() == '256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610'
 
-    h, _ = blake2b256([b'hello', b' world'])
+    h, _ = cry.blake2b256([b'hello', b' world'])
     assert h.hex() == '256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610'
 
 
 def test_keccak256():
-    h, _ = keccak256([b'hello world'])
+    h, _ = cry.keccak256([b'hello world'])
     assert h.hex() == '47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad'
 
-    h, _ = keccak256([b'hello', b' world'])
+    h, _ = cry.keccak256([b'hello', b' world'])
     assert h.hex() == '47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad'
 
 
@@ -34,8 +29,8 @@ def test_address():
     ]
 
     for addr in address:
-        assert is_address(addr)
-        assert to_checksum_address(addr) == addr
+        assert cry.is_address(addr)
+        assert cry.to_checksum_address(addr) == addr
 
 
 def test_private_key():
@@ -55,7 +50,7 @@ def test_derive_public_key():
 def test_public_key_to_address():
     pub = bytes.fromhex(
         '04b90e9bb2617387eba4502c730de65a33878ef384a46f1096d86f2da19043304afa67d0ad09cf2bea0c6f2d1767a9e62a7a7ecc41facf18f2fa505d92243a658f')
-    address = public_key_to_address(pub)
+    address = cry.public_key_to_address(pub)
     assert '0x' + address.hex() == '0xd989829d88b0ed1b06edf5c50174ecfa64f14a64'
 
 
@@ -64,12 +59,12 @@ def test_sign_hash():
         '04b90e9bb2617387eba4502c730de65a33878ef384a46f1096d86f2da19043304afa67d0ad09cf2bea0c6f2d1767a9e62a7a7ecc41facf18f2fa505d92243a658f')
     priv = bytes.fromhex(
         '7582be841ca040aa940fff6c05773129e135623e41acce3e0b8ba520dc1ae26a')
-    msg_hash, _ = keccak256([b'hello world'])
+    msg_hash, _ = cry.keccak256([b'hello world'])
 
-    sig = secp256k1.sign(msg_hash, priv)
+    sig = cry.secp256k1.sign(msg_hash, priv)
     assert sig.hex() == 'f8fe82c74f9e1f5bf443f8a7f8eb968140f554968fdcab0a6ffe904e451c8b9244be44bccb1feb34dd20d9d8943f8c131227e55861736907b02d32c06b934d7200'
 
-    _pub = secp256k1.recover(msg_hash, sig)
+    _pub = cry.secp256k1.recover(msg_hash, sig)
     assert _pub.hex() == pub.hex()
 
 
@@ -146,7 +141,7 @@ def test_hdnode():
         '2ac1a0aecd5c80fb5524348130ab7cf92670470a'
     ]
 
-    hd_node = HDNode.from_mnemonic(words)
+    hd_node = cry.HDNode.from_mnemonic(words)
 
     for idx, address in enumerate(addresses):
         child_node = hd_node.derive(idx)
@@ -156,13 +151,13 @@ def test_hdnode():
     pub = hd_node.public_key()
     cc = hd_node.chain_code()
 
-    n = HDNode.from_private_key(priv, cc)
+    n = cry.HDNode.from_private_key(priv, cc)
 
     for idx, address in enumerate(addresses):
         child_node = n.derive(idx)
         assert child_node.address().hex() == address
 
-    n2 = HDNode.from_public_key(pub, cc)
+    n2 = cry.HDNode.from_public_key(pub, cc)
 
     for idx, address in enumerate(addresses):
         child_node = n.derive(idx)
--- thor_devkit/cry/__init__.py
+++ thor_devkit/cry/__init__.py
@@ -1,4 +1,4 @@
-from .blake2b import blake2b256
-from .keccak import keccak256
 from .address import is_address, public_key_to_address, to_checksum_address
+from .blake2b import blake2b256
 from .hdnode import HDNode
+from .keccak import keccak256
