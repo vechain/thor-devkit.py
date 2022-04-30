7e27a092c903fcd2a83f809efd94c4efd81ea90e
STerliakov <terlya.stas@gmail.com>
2022-04-30 23:50:38 +0300

    Better coverage config, deprecation code doesn't need to be tested

--- thor_devkit/deprecation.py
+++ thor_devkit/deprecation.py
@@ -4,7 +4,7 @@ from typing import Callable, TypeVar
 _C = TypeVar("_C", bound=Callable)
 
 
-def deprecated_to_property(func: _C) -> _C:
+def deprecated_to_property(func: _C) -> _C:  # pragma: no cover
     def inner(*args, **kwargs):
         warnings.warn(
             DeprecationWarning("This method is deprecated, use property instead")
