21fd6468e1f332b094cd434f7bb9b5b815d16f57
STerliakov <terlya.stas@gmail.com>
2022-05-03 00:55:42 +0300

    Switch to modern PEP518 (PEP621) pyproject.toml to keep all configs and setup data.

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
 install:
 	python3 -m venv .env
-	. .env/bin/activate && python3 -m pip install -r dev_requirements.txt
+	. .env/bin/activate && python3 -m pip install -e .
 
 test:
-	. .env/bin/activate \
-		&& python3 -m pytest --cov=thor_devkit --no-cov-on-fail --cov-report=term-missing -vv -s
+	. .env/bin/activate && python3 -m pytest -vv -s
 
 publish: test
 	rm -rf dist/*
-	. .env/bin/activate && python3 setup.py sdist bdist_wheel
-	. .env/bin/activate && python3 -m twine upload dist/*
+	. .env/bin/activate \
+		&& python3 setup.py sdist bdist_wheel \
+		&& python3 -m twine upload dist/*
--- setup.py
+++ setup.py
@@ -1,36 +1,4 @@
+"""This file is needed only to allow editable installs"""
 import setuptools
 
-long_description = ""
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-assert long_description
-
-with open("requirements.txt", "r") as req:
-    requirements = [x.strip() for x in req]
-
-setuptools.setup(
-    name="thor-devkit",
-    version="1.0.12",
-    author="laalaguer",
-    author_email="laalaguer@gmail.com",
-    description="SDK to interact with VeChain Thor public blockchain.",
-    keywords="vechain thor blockchain sdk",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Intended Audience :: Developers",
-        "Topic :: Software Development :: Build Tools",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
-        "Operating System :: OS Independent",
-    ],
-    url="https://github.com/laalaguer/thor-devkit.py",
-    project_urls={
-        "Documentation": "https://github.com/laalaguer/thor-devkit.py",
-        "Source": "https://github.com/laalaguer/thor-devkit.py",
-        "Issue Tracker": "https://github.com/laalaguer/thor-devkit.py/issues",
-    },
-    python_requires=">=3.6",
-    install_requires=requirements,
-    packages=setuptools.find_packages(),
-)
+setuptools.setup()
