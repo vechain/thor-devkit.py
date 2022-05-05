b20a7039eb7cac3b78c546578e3d88349a75f709
STerliakov <terlya.stas@gmail.com>
2022-05-05 14:53:23 +0300

    Fix tests to use fixtures

--- tests/test_cry.py
+++ tests/test_cry.py
@@ -1,110 +1,113 @@
+import re
+
+import pytest
+
 from thor_devkit import cry
 from thor_devkit.cry import keystore, mnemonic, secp256k1, utils
 
 
-def test_utils():
-    address = [
+@pytest.fixture(
+    params=[
         "0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed",
         "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359",
         "0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB",
-        "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb",
+        "0XD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb",
     ]
+)
+def address(request):
+    return request.param
 
-    for addr in address:
-        assert not utils.remove_0x(addr).startswith("0x")
 
-    # no 0x at all
-    assert (
-        utils.remove_0x("D1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb")
-        == "D1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb"
+@pytest.fixture()
+def public_key():
+    return bytes.fromhex(
+        "04b90e9bb2617387eba4502c730de65a33878ef384a46f1096d86f2da19043304"
+        "afa67d0ad09cf2bea0c6f2d1767a9e62a7a7ecc41facf18f2fa505d92243a658f"
     )
 
-    # 0x in the middle
-    assert (
-        utils.remove_0x("D1220x0A0cf47c7B9Be7A2E6BA89F429762e7b9aDb")
-        == "D1220x0A0cf47c7B9Be7A2E6BA89F429762e7b9aDb"
+
+@pytest.fixture()
+def private_key():
+    return bytes.fromhex(
+        "7582be841ca040aa940fff6c05773129e135623e41acce3e0b8ba520dc1ae26a"
     )
+
+
+@pytest.fixture()
+def seed_phrase():
+    return (
+        "ignore empty bird silly journey junior ripple have guard waste between tenant"
+    )
+
+
+def test_remove_0x_1(address: str):
+    assert utils.remove_0x(address)[:2] not in {"0x", "0X"}
+
+
+def test_remove_0x_2():
+    # no 0x at all, same length
+    p = "D1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb"
+    assert utils.remove_0x(p) == p
+
+    # 0x in the middle, other length
+    p = "12A10x12"
+    assert utils.remove_0x(p) == p
 
 
 def test_blake2b():
+    expected = "256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610"
+
     h, _ = cry.blake2b256([b"hello world"])
-    assert h.hex() == "256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610"
+    assert h.hex() == expected
 
     h, _ = cry.blake2b256([b"hello", b" world"])
-    assert h.hex() == "256c83b297114d201b30179f3f0ef0cace9783622da5974326b436178aeef610"
+    assert h.hex() == expected
 
 
 def test_keccak256():
+    expected = "47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad"
+
     h, _ = cry.keccak256([b"hello world"])
-    assert h.hex() == "47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad"
+    assert h.hex() == expected
 
     h, _ = cry.keccak256([b"hello", b" world"])
-    assert h.hex() == "47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad"
+    assert h.hex() == expected
 
 
-def test_address():
-    address = [
-        "0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed",
-        "0xfB6916095ca1df60bB79Ce92cE3Ea74c37c5d359",
-        "0xdbF03B407c01E7cD3CBea99509d93f8DDDC8C6FB",
-        "0xD1220A0cf47c7B9Be7A2E6BA89F429762e7b9aDb",
-    ]
-
-    for addr in address:
-        assert cry.is_address(addr)
-        assert cry.to_checksum_address(addr) == addr
+def test_address(address: str):
+    assert cry.is_address(address)
+    assert cry.to_checksum_address(address) == re.sub(r"^(0X)", r"0x", address)
 
 
-def test_private_key():
+def test_private_key_length(private_key: bytes):
     private_key = secp256k1.generate_private_key()
     assert len(private_key) == 32
 
 
-def test_derive_public_key():
-    priv = bytes.fromhex(
-        "7582be841ca040aa940fff6c05773129e135623e41acce3e0b8ba520dc1ae26a"
-    )
-    pub = bytes.fromhex(
-        "04b90e9bb2617387eba4502c730de65a33878ef384a46f1096d86f2da19043304"
-        "afa67d0ad09cf2bea0c6f2d1767a9e62a7a7ecc41facf18f2fa505d92243a658f"
-    )
-    _pub = secp256k1.derive_public_key(priv)
-    assert pub.hex() == _pub.hex()
+def test_derive_public_key(public_key: bytes, private_key: bytes):
+    _pub = secp256k1.derive_public_key(private_key)
+    assert public_key.hex() == _pub.hex()
 
 
-def test_public_key_to_address():
-    pub = bytes.fromhex(
-        "04b90e9bb2617387eba4502c730de65a33878ef384a46f1096d86f2da19043304"
-        "afa67d0ad09cf2bea0c6f2d1767a9e62a7a7ecc41facf18f2fa505d92243a658f"
-    )
-    address = cry.public_key_to_address(pub)
+def test_public_key_to_address(public_key: bytes):
+    address = cry.public_key_to_address(public_key)
     assert "0x" + address.hex() == "0xd989829d88b0ed1b06edf5c50174ecfa64f14a64"
 
 
-def test_sign_hash():
-    pub = bytes.fromhex(
-        "04b90e9bb2617387eba4502c730de65a33878ef384a46f1096d86f2da19043304"
-        "afa67d0ad09cf2bea0c6f2d1767a9e62a7a7ecc41facf18f2fa505d92243a658f"
-    )
-    priv = bytes.fromhex(
-        "7582be841ca040aa940fff6c05773129e135623e41acce3e0b8ba520dc1ae26a"
-    )
+def test_sign_hash(public_key: bytes, private_key: bytes):
     msg_hash, _ = cry.keccak256([b"hello world"])
 
-    sig = cry.secp256k1.sign(msg_hash, priv)
+    sig = cry.secp256k1.sign(msg_hash, private_key)
     assert sig.hex() == (
         "f8fe82c74f9e1f5bf443f8a7f8eb968140f554968fdcab0a6ffe904e451c8b924"
         "4be44bccb1feb34dd20d9d8943f8c131227e55861736907b02d32c06b934d7200"
     )
 
     _pub = cry.secp256k1.recover(msg_hash, sig)
-    assert _pub.hex() == pub.hex()
+    assert _pub.hex() == public_key.hex()
 
 
-def test_mnemonic():
-    SENTENCE = (
-        "ignore empty bird silly journey junior ripple have guard waste between tenant"
-    )
+def test_mnemonic(seed_phrase):
     SEED = (
         "28bc19620b4fbb1f8892b9607f6e406fcd8226a0d6dc167ff677d122a1a64ef9"
         "36101a644e6b447fd495677f68215d8522c893100d9010668614a68b3c7bb49f"
@@ -116,7 +119,7 @@ def test_mnemonic():
     assert len(_words) == 12
 
     # Valid: True
-    words = SENTENCE.split(" ")
+    words = seed_phrase.split(" ")
     assert mnemonic.is_valid(words)
 
     # Valid: True
@@ -127,7 +130,7 @@ def test_mnemonic():
     assert not mnemonic.is_valid(words2)
 
     # Valid: False
-    words3 = sorted(SENTENCE.split(" "))
+    words3 = sorted(seed_phrase.split(" "))
     assert not mnemonic.is_valid(words3)
 
     # Seed generated from words.
@@ -168,11 +171,8 @@ def test_keystore():
     assert _priv.hex() == private_key_hex
 
 
-def test_hdnode():
-    sentence = (
-        "ignore empty bird silly journey junior ripple have guard waste between tenant"
-    )
-    words = sentence.split(" ")
+def test_hdnode(seed_phrase):
+    words = seed_phrase.split(" ")
 
     addresses = [
         "339fb3c438606519e2c75bbf531fb43a0f449a70",
